92aa867b50790f43f92efd8422d881886ca2c1b9
RanjithMasterM <ranjithm261998@gmail.com>
2025-09-19 19:09:38 +0530

    First time Push

---
+++ src/test/java/org/tcs/Sample.java
@@ -0,0 +1,16 @@
+package org.tcs;
+
+public class Sample {
+
+	private void test1() {
+System.out.println("Test 1");
+	}
+	private void test2() {
+		System.out.println("Test 2");
+			}
+	private void test3() {
+		System.out.println("Test 3");
+			}
+	
+	
+}
